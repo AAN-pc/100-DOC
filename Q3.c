7569969bd4b6c59835bab3d04d5fe4aa23f5f0c8
AAN-pc <aanyaa.gilll@gmail.com>
2025-09-22 16:00:00 +0530

    Area and Perimeter

---
+++ Q3.c
@@ -0,0 +1,8 @@
+// Q3: Area and Perimeter of Rectangle
+#include <stdio.h>
+int main() {
+    float l, b;
+    scanf("%f%f", &l, &b);
+    printf("Area = %.2f\nPerimeter = %.2f", l*b, 2*(l+b));
+    return 0;
+}
